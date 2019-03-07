6d9c63ff06114ea50afbd82484d1eafd2ce67bbe
Dmitry Mpower <mp0wer@users.noreply.github.com>
2019-03-07 16:43:22 +0300

    Update browser-version-test.js

--- browser-version-test.js
+++ browser-version-test.js
@@ -3,5 +3,5 @@ var isValidBrowser = bowser.chrome&&bowser.version>=63||bowser.msie&&bowser.vers
 if (!isValidBrowser) {
   alert("Unsupported browser " + bowser.name + " version " + bowser.version);
 } else {
-  alert("Great! You have supported browser " + bowser.name + " version " + bowser.version + ". Samsung browser - " + bowser.samsungBrowser ? "yes" : "no");
+  alert("Great! You have supported browser " + bowser.name + " version " + bowser.version + ". Samsung browser - " + (bowser.samsungBrowser ? "yes" : "no"));
 }
