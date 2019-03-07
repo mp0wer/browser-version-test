ab8a50c2963e3efadf0df8e42586702f48b61f25
Dmitry Mpower <mp0wer@users.noreply.github.com>
2019-03-07 13:31:16 +0300

    Update browser-version-test.js

--- browser-version-test.js
+++ browser-version-test.js
@@ -1,2 +1,3 @@
+const Bowser = require('bowser/bundled');
 const browser = Bowser.getParser(window.navigator.userAgent);
 alert(`Browser "${browser.getBrowserName()}", version "${browser.getBrowserVersion()}"`);
