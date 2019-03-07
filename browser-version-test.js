529238f3cd44d95f0e1c742072860a418d4844ae
Dmitry Mpower <mp0wer@users.noreply.github.com>
2019-03-07 15:58:54 +0300

    Update browser-version-test.js

--- browser-version-test.js
+++ browser-version-test.js
@@ -1,3 +1,7 @@
-const Bowser = module.exports;
-const browser = Bowser.getParser(window.navigator.userAgent);
-alert(`Browser "${browser.getBrowserName()}", version "${browser.getBrowserVersion()}"`);
+var bowser = module.exports;
+var isValidBrowser = bowser.chrome&&bowser.version>=63||bowser.msie&&bowser.version>=11||bowser.msedge||bowser.safari&&bowser.version>=9||bowser.ios||bowser.blink;
+if (!isValidBrowser) {
+  alert("Unsupported browser " + bowser.name + " version " + bowser.version);
+} else {
+  alert("Great! You have supported browser")
+}
